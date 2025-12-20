7601938a8fb345806336e18eea8a5e85fe16480c
hanumoka <amagramer@gmail.com>
2025-12-20 16:29:45 +0900

    1

---
+++ src/main/java/com/hanumoka/sado/SadoBeApplication.java
@@ -0,0 +1,13 @@
+package com.hanumoka.sado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SadoBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SadoBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanumoka/sado/SadoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanumoka.sado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SadoBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
